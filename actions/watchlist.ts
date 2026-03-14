d0b4d24017a178ce8015b2b32b23f17f2735e0be
Supratik Ray <raysupratik8@gmail.com>
2026-03-14 23:48:04 +0530

    created addToWatchList server action

---
+++ actions/watchlist.ts
@@ -0,0 +1,27 @@
+"use server"
+
+import { watchlistTable } from "@/db/schema"
+import { db } from "@/lib/db"
+import { auth } from "@clerk/nextjs/server"
+
+type WatchStatus = "plan-to-watch" | "watching" | "watched"
+
+type WatchData = {
+  mediaId: string
+  mediaType: "movie" | "tv"
+  mediaTitle: string
+  mediaPoster: string | undefined
+  status: WatchStatus
+}
+
+export async function addToWatchList(watchData: WatchData) {
+  try {
+    const { userId } = await auth()
+    if (!userId) return { success: false, message: "Unauthorized!" }
+    await db.insert(watchlistTable).values({ ...watchData, userId })
+    return { success: true, message: "Successfully added to watchlist!" }
+  } catch (error) {
+    console.error(error)
+    return { success: false, message: "Failed to add to watchlist!" }
+  }
+}
